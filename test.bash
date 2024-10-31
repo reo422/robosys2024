ce63ae69ffcf6c2f730917f1d60f2b199ea90662
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2024-10-31 12:43:58 +0900

    Add test

--- test.bash
+++ test.bash
@@ -7,8 +7,18 @@ ng () {
 
 res=0
 
+### 正常な動作###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+###変な動作###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
